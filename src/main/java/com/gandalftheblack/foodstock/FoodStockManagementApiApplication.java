fdf0abd4309efb65692adf3728e774b6514b6068
GandalfTheBlack16 <juanitonavarro90@gmail.com>
2023-08-16 16:16:22 +0200

    Updated application port config to enable deployment

--- src/main/java/com/gandalftheblack/foodstock/FoodStockManagementApiApplication.java
+++ src/main/java/com/gandalftheblack/foodstock/FoodStockManagementApiApplication.java
@@ -3,11 +3,16 @@ package com.gandalftheblack.foodstock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class FoodStockManagementApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FoodStockManagementApiApplication.class, args);
+        SpringApplication app = new SpringApplication(FoodStockManagementApiApplication.class);
+        String port = System.getenv("PORT");
+        app.setDefaultProperties(Collections.singletonMap("server.port", port == null ? "8080": port));
+        app.run(args);
     }
 
 }
